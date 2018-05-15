6fedd117c86ec8b9bcaf10ddbbdfed425f06873d
Niranjan <kniranjankumar.eee@gmail.com>
2018-05-15 11:49:19 -0400

    cnn predictor

--- baselines/ppo2/supervised_lstm.py
+++ baselines/ppo2/supervised_lstm.py
@@ -6,9 +6,9 @@ import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
 import gym
 
-env = gym.make('DartBlockPush-v0')
+env = gym.make('DartBlockPush-v1')
 # env.reset()
-mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+
 
 # define constants
 # unrolled through 28 time steps
@@ -40,6 +40,35 @@ def simple_fc_network(x):
     return prediction
 
 
+def cnn_network(x, actions, is_training):
+    dropout = 0.25
+    input = tf.unstack(x, time_steps, axis=1)
+    conv1 = tf.layers.conv2d(input, 32, 5, activation=tf.nn.relu)
+    # Max Pooling (down-sampling) with strides of 2 and kernel size of 2
+    conv1 = tf.layers.max_pooling2d(conv1, 2, 2)
+
+    # Convolution Layer with 64 filters and a kernel size of 3
+    conv2 = tf.layers.conv2d(conv1, 64, 3, activation=tf.nn.relu)
+    # Max Pooling (down-sampling) with strides of 2 and kernel size of 2
+    conv2 = tf.layers.max_pooling2d(conv2, 2, 2)
+
+    # Flatten the data to a 1-D vector for the fully connected layer
+    fc1 = tf.contrib.layers.flatten(conv2)
+
+    # Fully connected layer (in tf contrib folder for now)
+    fc1 = tf.layers.dense(fc1, 1024)
+    fc2 = tf.layers.dense(actions, 20)
+    features = tf.concat([fc1,fc2], axis=2)
+    lstm_layer = rnn.BasicLSTMCell(num_units, forget_bias=1)
+    outputs, _ = rnn.static_rnn(lstm_layer, fc1, dtype=tf.float32)
+    # Apply Dropout (if is_training is False, dropout is not applied)
+    fc1 = tf.layers.dropout(fc1, rate=dropout, training=is_training)
+
+        # Output layer, class prediction
+    out = tf.layers.dense(fc1, n_classes)
+    prediction = tf.layers.dense(inputs=out[-1], units=1)
+    prediction = tf.identity(prediction, name='predict')
+    return prediction
 # prediction=tf.matmul(outputs[-1],out_weights)+out_bias
 # loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction,labels=y))
 prediction = simple_fc_network(x)
@@ -74,11 +103,12 @@ with tf.Session() as sess:
     mb_obs, mb_action, y1, x1 = [], [], [], []
     obs = env.reset()
     obs = env.reset()
-    mb_obs.append(obs['observation'])
+    img = env.render('rgb_array')
+    mb_obs.append(img)
     for i in range(30000):
         action = np.random.uniform(-1, 1, 2)
         obs, _, done, _ = env.step(np.concatenate([action, np.random.uniform(0.5, 4, 1)]))
-        mb_obs.append(np.concatenate([obs['observation'], action]))
+        mb_obs.append(np.concatenate([img, action]))
         mb_action.append(action)
         if (done):
             y1.append([obs['mass']])
