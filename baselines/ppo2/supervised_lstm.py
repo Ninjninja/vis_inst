3745e004eb2855adb83ac158f7fc1a8e2ce4da99
Niranjan <kniranjankumar.eee@gmail.com>
2018-05-18 18:10:44 -0400

    supervised cnn

--- baselines/ppo2/supervised_lstm.py
+++ baselines/ppo2/supervised_lstm.py
@@ -5,6 +5,7 @@ import numpy as np
 # import mnist dataset
 from tensorflow.examples.tutorials.mnist import input_data
 import gym
+import cv2 as cv
 from tensorflow.python import debug as tf_debug
 
 env = gym.make('DartBlockPush-v0')
@@ -29,7 +30,7 @@ batch_size = 128
 x = tf.placeholder(tf.float32, [None, time_steps, 256, 256, 3], name='x')
 act = tf.placeholder(tf.float32, [None, time_steps, 2], name='action')
 y = tf.placeholder(tf.float32, [None, n_classes])
-
+phase = tf.placeholder(tf.bool, name='phase')
 
 def simple_fc_network(input,act):
     with tf.variable_scope('fc_model'):
@@ -45,35 +46,41 @@ def simple_fc_network(input,act):
         return prediction
 
 
-def cnn_network(input, actions):
-    with tf.variable_scope('cnn_model'):
+def cnn_network(input, actions, istraining):
+    with tf.variable_scope('cnn_model', reuse=tf.AUTO_REUSE):
         dropout = 0.25
         input1 = tf.reshape(input, shape=[-1,256,256,3])
         actions1 = tf.reshape(actions, shape = [-1,2])
         conv1 = tf.layers.conv2d(input1, 32, 16, activation=tf.nn.relu,kernel_initializer=tf.random_normal_initializer)
+        conv1_n = tf.layers.batch_normalization(conv1, training=istraining)
+
         # Max Pooling (down-sampling) with strides of 2 and kernel size of 2
-        conv1 = tf.layers.max_pooling2d(conv1, 8, 8)
+        conv1 = tf.layers.max_pooling2d(conv1_n, 8, 8)
 
         # Convolution Layer with 64 filters and a kernel size of 3
         conv2 = tf.layers.conv2d(conv1, 64, 8, activation=tf.nn.relu,kernel_initializer=tf.random_normal_initializer)
+        conv2_n = tf.layers.batch_normalization(conv2, training=istraining)
+
         # Max Pooling (down-sampling) with strides of 2 and kernel size of 2
-        conv2 = tf.layers.max_pooling2d(conv2, 4, 4)
+        conv2 = tf.layers.max_pooling2d(conv2_n, 4, 4)
 
         conv3 = tf.layers.conv2d(conv2, 64, 4, activation=tf.nn.relu,kernel_initializer=tf.random_normal_initializer)
+        conv3_n = tf.layers.batch_normalization(conv3, training=istraining)
+
         # Max Pooling (down-sampling) with strides of 2 and kernel size of 2
-        conv3 = tf.layers.max_pooling2d(conv3, 2, 2)
+        conv3_n = tf.layers.max_pooling2d(conv3_n, 2, 2)
         # Flatten the data to a 1-D vector for the fully connected layer
-        fc1 = tf.contrib.layers.flatten(conv3)
+        fc1 = tf.contrib.layers.flatten(conv3_n)
 
         # Fully connected layer (in tf contrib folder for now)
-        fc1 = tf.layers.dense(fc1, 100)
+        fc1 = tf.layers.dense(fc1, 300)
         fc2 = tf.layers.dense(actions1, 20,activation=tf.nn.relu)
         features = tf.concat([fc1,fc2], axis=1)
-        features = tf.reshape(features,shape=[-1,time_steps,120])
+        features = tf.reshape(features,shape=[-1,time_steps,320])
         features = tf.unstack(features, time_steps, axis=1)
         lstm_layer = rnn.BasicLSTMCell(num_units, forget_bias=1,name='lstm_output')
         outputs, _ = rnn.static_rnn(lstm_layer, features, dtype=tf.float32)
-        prediction = tf.layers.dense(inputs=outputs[-1], units=1,name='pred_fc')
+        prediction = tf.layers.dense(inputs=outputs[-1], units=1,name='pred_fc',activation=tf.nn.relu)
         # prediction = tf.gather(prediction, [1], axis=)
             # Output layer, class prediction
         # prediction =tf.gather(prediction,[num_units-1],axis=0)
@@ -82,8 +89,10 @@ def cnn_network(input, actions):
         return prediction
 # prediction=tf.matmul(outputs[-1],out_weights)+out_bias
 # loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction,labels=y))
-prediction = cnn_network(x, act)
+prediction = cnn_network(x, act,phase)
 loss = tf.reduce_mean(tf.losses.mean_squared_error(prediction, y))
+update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+# with tf.control_dependencies(update_ops):
 opt = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss, name='min_loss')
 
 # correct prediction
@@ -111,22 +120,26 @@ with tf.Session() as sess:
     #         print("Loss ",los)
     #         print("__________________")
     mb_obs, mb_action, y1, x1, x1_action = [], [], [], [], []
-
+    los = 0
     obs = env.reset()
     obs = env.reset()
 
     for i in range(300):
         count = 0
-        while count < 50:
+        while count < 1:
             action = np.random.uniform(-1, 1, 2)
-            obs, _, done, _ = env.step(np.concatenate([action, np.random.uniform(1, 4, 1)]))
+            obs, rew, done, _ = env.step(np.concatenate([action, np.random.uniform(1, 4, 1)]))
+
             mb_obs.append(obs['observation'])
             mb_action.append(action)
             if (done):
+                # print(rew, i)
                 count += 1
                 y1.append([obs['mass']])
                 x1.append(np.array(mb_obs[:-1]))
                 x1_action.append(np.array(mb_action[:-1]))
+                # join = np.concatenate([mb_obs[0], mb_obs[1]], axis=1)
+                # cv.imwrite('./train_imgs/eps_'+str(count+i*200)+'.jpg',join)
                 obs = env.reset()
                 mb_obs.append(obs['observation'])
                 mb_action.append(action)
@@ -138,25 +151,31 @@ with tf.Session() as sess:
         Y_train = np.array(y1)
         y1 = []
         # sess = tf_debug.LocalCLIDebugWrapperSession(sess)
-        for i in range(200):
-            los, _, _ = sess.run([loss, opt, merged], feed_dict={x: X_train, y: Y_train, act:X_train_act})
-            print(los)
+
+        for j in range(10):
+
+            if i < 100:
+                los, prediction, _,_, _ = sess.run([loss, prediction, opt, update_ops, merged], feed_dict={x: X_train, y: Y_train, act:X_train_act, phase: True})
+                if j==0:
+                    print(los)
+            else:
+                if i == 100:
+                    print('test')
+
+                # sess = tf_debug.LocalCLIDebugWrapperSession(sess)
+                los, _ = sess.run([loss, merged], feed_dict={x: X_train, y: Y_train, act: X_train_act, phase: False})
+                if j==0:
+                    print(los)
+                break
+
+
         x1 = []
         x1_action = []
 
-    X_test = np.array(x1[8000:])
-    Y_test = np.array(y1[8000:])
+    # X_test = np.array(x1[8000:])
+    # Y_test = np.array(y1[8000:])
     # np.save('X_train', X_train)
     # np.save('Y_train', Y_train)
-    for i in range(20000):
-        sess.run(opt, feed_dict={x: X_train, y: Y_train, act:X_train_act})
-        if i % 10 == 0:
-            # acc = sess.run(accuracy, feed_dict={x: X, y: Y})
-            los, summary_out = sess.run([loss, merged], feed_dict={x: X_test, y: Y_test})
-            train_writer.add_summary(summary_out, i)
-            print("For iter ", iter)
-            # print("Accuracy ", acc)
-            print("Loss ", los)
-            print("__________________")
 
-    saver.save(sess, './mass_predict_cnn', global_step=20000)
+
+    saver.save(sess, './mass_predict_cnn_2', global_step=20000)
--- baselines/ppo2/supervised_lstm_test.py
+++ baselines/ppo2/supervised_lstm_test.py
@@ -5,15 +5,17 @@ import numpy as np
 env = gym.make('DartBlockPush-v0')
 
 mb_obs = []
+mb_action = []
 obs = env.reset()
 action = np.random.uniform(-1, 1, 2)
-mb_obs.append(np.concatenate([obs['observation'], action * 0]))
-def predict(obs_seq):
+mb_obs.append(obs['observation'])
+mb_action.append(action*0)
+def predict(obs_seq, act_seq):
     # sess = tf.get_default_session()
     sess = tf.get_default_session()
     # First let's load meta graph and restore weights
     saver = tf.train.import_meta_graph(
-        '/home/niranjan/Projects/vis_inst/baselines/baselines/ppo2/mass_predict-20000.meta')
+        '/home/niranjan/Projects/vis_inst/baselines/baselines/ppo2/mass_predict_cnn-20000.meta')
     saver.restore(sess, tf.train.latest_checkpoint('/home/niranjan/Projects/vis_inst/baselines/baselines/ppo2/'))
 
     # Now, let's access and create placeholders variables and
@@ -21,27 +23,32 @@ def predict(obs_seq):
 
     graph = tf.get_default_graph()
     x = graph.get_tensor_by_name("x:0")
-    op_to_restore = graph.get_tensor_by_name("predict:0")
-    feed_dict = {x: np.expand_dims(obs_seq, axis=0)}
+    act = graph.get_tensor_by_name("action:0")
+    phase = graph.get_tensor_by_name("phase:0")
+    op_to_restore = graph.get_tensor_by_name("cnn_model/predict:0")
+    feed_dict = {x: np.expand_dims(obs_seq, axis=0),act: np.expand_dims(act_seq, axis=0), phase:True}
     # print(sess.run(op_to_restore, feed_dict))
     return sess.run(op_to_restore, feed_dict)
 
-# with tf.Session() as sess:
-#     for i in range(60):
-#         action = np.array([0.5,0.5])#np.random.uniform(-1, 1, 2)
-#         obs, _, done, _ = env.step(np.concatenate([action, np.random.uniform(0.5, 4, 1)]))
-#
-#         mb_obs.append(np.concatenate([obs['observation'], action]))
-#         if (i%3==1):
-#             print(obs['mass'])
-#             obs_seq = np.array(mb_obs[-2:])
-#             print(predict(obs_seq))
-#
-#         if (done):
-#             obs = env.reset()
-#             action = np.random.uniform(-1, 1, 2)
-#             mb_obs = []
-#
+with tf.Session() as sess:
+    for i in range(60):
+        # action = np.array([0.5,0.5])#
+        action = np.random.uniform(-1, 1, 2)
+        obs, _, done, _ = env.step(np.concatenate([action, np.random.uniform(1, 4, 1)]))
+
+        mb_obs.append(obs['observation'])
+        mb_action.append(action)
+        if (i%3==1):
+            print(obs['mass'])
+            obs_seq = np.array(mb_obs[-2:])
+            action_seq = np.array(mb_action[-2:])
+            print(predict(obs_seq,action_seq))
+
+        if (done):
+            obs = env.reset()
+            action = np.random.uniform(-1, 1, 2)
+            mb_obs = []
+
 
 # Now, access the op that you want to run.
 
