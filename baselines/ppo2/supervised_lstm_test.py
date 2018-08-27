a86e9d887e8365527b311a80ff365272641450e1
Niranjan <kniranjankumar.eee@gmail.com>
2018-08-27 15:16:34 -0400

    'added supervised dynamic model'

--- baselines/ppo2/supervised_lstm_test.py
+++ baselines/ppo2/supervised_lstm_test.py
@@ -2,14 +2,15 @@ import tensorflow as tf
 import gym
 import numpy as np
 import cv2 as cv
-# env = gym.make('DartBlockPush-v0')
-#
-# mb_obs = []
-# mb_action = []
-# obs = env.reset()
-# action = np.random.uniform(-1, 1, 2)
-# mb_obs.append(obs['observation'])
-# mb_action.append(action*0)
+
+env = gym.make('DartBlockPush-v0')
+
+mb_obs = []
+mb_action = []
+obs = env.reset()
+action = np.random.uniform(-1, 1, 2)
+mb_obs.append(obs['observation'])
+mb_action.append(action * 0)
 def predict(obs_seq, act_seq):
     # sess = tf.get_default_session()
     sess = tf.get_default_session()
@@ -29,41 +30,42 @@ def predict(obs_seq, act_seq):
     feed_dict = {x: np.expand_dims(obs_seq, axis=0),act: np.expand_dims(act_seq, axis=0), phase:False}
     # print(sess.run(op_to_restore, feed_dict))
     return sess.run(op_to_restore, feed_dict)
-#
-# with tf.Session() as sess:
-#     error  = []
-#     pred_out = 0
-#     np.random.seed(100)
-#     for i in range(60):
-#         # action = np.array([0.5,0.5, 0.5])#
-#
-#         action = np.random.uniform(-1, 1, 3)
-#         # action = np.random.normal(0,0.5,3)
-#         action = np.clip(action,-1,1)
-#         action2 = np.copy(action[:-1])
-#         action[2] = pred_out
-#         obs, rew, done, _ = env.step(action)
-#
-#         mb_obs.append(obs['observation'])
-#         mb_action.append(action2)
-#         print('action',action2)
-#         if (i+2)%3==0 and i>1:
-#             # print(i)
-#             # print(obs['mass'])
-#             obs_seq = np.array(mb_obs)
-#             action_seq = np.array(mb_action)
-#             cv.imwrite('obs.jpg',mb_obs[-1])
-#             pred_out = predict(obs_seq,action_seq)
-#             error.append(pred_out - obs['mass'])
-#
-#         if (done):
-#             print(rew)
-#             obs = env.reset()
-#             action = np.random.uniform(-1, 1, 2)
-#             mb_obs = []
-#             mb_action = []
-#     pred_out = np.array(pred_out)
-#     print(np.mean(np.abs(error)))
+
+
+with tf.Session() as sess:
+    error = []
+    pred_out = 0
+    np.random.seed(100)
+    for i in range(60):
+        # action = np.array([0.5,0.5, 0.5])#
+
+        action = np.random.uniform(-1, 1, 6)
+        # action = np.random.normal(0,0.5,3)
+        action = np.clip(action, -1, 1)
+        action2 = np.copy(action[:-1])
+        action[2] = pred_out
+        obs, rew, done, _ = env.step(action)
+
+        mb_obs.append(obs['observation'])
+        mb_action.append(action2)
+        print('action', action2)
+        if (i + 2) % 3 == 0 and i > 1:
+            # print(i)
+            # print(obs['mass'])
+            obs_seq = np.array(mb_obs)
+            action_seq = np.array(mb_action)
+            cv.imwrite('obs.jpg', mb_obs[-1])
+            pred_out = predict(obs_seq, action_seq)
+            error.append(pred_out - obs['mass'])
+
+        if (done):
+            print(rew)
+            obs = env.reset()
+            action = np.random.uniform(-1, 1, 2)
+            mb_obs = []
+            mb_action = []
+    pred_out = np.array(pred_out)
+    print(np.mean(np.abs(error)))
 
 # Now, access the op that you want to run.
 
